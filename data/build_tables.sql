a9082b40425fc2dc51025f54fb5428e3996df080
Shawn <shawnesseltine@hotmail.com>
2021-01-05 00:47:56 -0500

    initial schema creation

---
+++ data/build_tables.sql
@@ -0,0 +1,50 @@
+CREATE SCHEMA IF NOT EXISTS TriageData;
+CREATE TABLE TriageData.Clinic (
+    id          SERIAL PRIMARY KEY,
+    name        varchar(255) NOT NULL
+);
+CREATE TABLE TriageData.Users (
+    id          SERIAL PRIMARY KEY,
+    clinic_id   integer,
+    username    varchar(40),
+    password    varchar,
+    salt        varchar,
+    admin       boolean,
+    CONSTRAINT fk_clinic
+        FOREIGN KEY(clinic_id)
+            REFERENCES TriageData.Clinic(id)
+);
+CREATE TABLE TriageData.TriageClasses (
+    id          SERIAL PRIMARY KEY,
+    clinic_id   integer,
+    severity    integer,
+    name        varchar(255),
+    duration    integer,
+    proportion  float,
+    CONSTRAINT fk_clinic
+        FOREIGN KEY(clinic_id)
+            REFERENCES TriageData.Clinic(id)
+);
+CREATE TABLE TriageData.Models (
+    id          SERIAL PRIMARY KEY,
+    data        varchar,
+    clinic_id   integer,
+    accuracy    float,
+    created     DATE NOT NULL DEFAULT CURRENT_DATE,
+    in_use      boolean,
+    CONSTRAINT fk_clinic
+        FOREIGN KEY(clinic_id)
+            REFERENCES TriageData.Clinic(id)
+);
+CREATE TABLE TriageData.Schedules (
+    id          SERIAL PRIMARY KEY,
+    data        varchar,
+    clinic_id   integer,
+    CONSTRAINT fk_clinic
+        FOREIGN KEY(clinic_id)
+            REFERENCES TriageData.Clinic(id)
+);
+CREATE INDEX username_idx ON TriageData.Users (username);
+CREATE INDEX clinic_schedule_idx ON TriageData.Schedules (clinic_id);
+CREATE INDEX clinic_models_idx ON TriageData.Models (clinic_id);
+CREATE INDEX clinic_triage_classes_idx ON TriageData.TriageClasses (clinic_id);
