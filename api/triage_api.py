670cc2f5d68a1f03d907f549cf5b230bdad50691
Daniel <rubinstd@mcmaster.ca>
2021-01-06 16:49:42 -0500

    Docker setup for api

--- api/triage_api.py
+++ api/triage_api.py
@@ -3,7 +3,7 @@
 ''' Read the documentation from Flask: https://flask-restful.readthedocs.io/en/latest/'''
 from flask import Flask
 from flask_restful import Api
-from api.resources.predict import Predict
+from resources.predict import Predict
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,4 +11,4 @@ api = Api(app)
 api.add_resource(Predict, '/predict')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
