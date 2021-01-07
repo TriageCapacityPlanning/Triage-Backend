e801e18730e048b69b7686d476db50873221352e
Shawn <shawnesseltine@hotmail.com>
2021-01-07 13:28:19 -0500

    adjust schema for efficiency

--- data/build_tables.sql
+++ data/build_tables.sql
@@ -1,26 +1,26 @@
+DROP SCHEMA IF EXISTS TriageData CASCADE;
 CREATE SCHEMA IF NOT EXISTS TriageData;
 CREATE TABLE TriageData.Clinic (
     id          SERIAL PRIMARY KEY,
     name        varchar(255) NOT NULL
 );
 CREATE TABLE TriageData.Users (
-    id          SERIAL PRIMARY KEY,
-    clinic_id   integer,
-    username    varchar(40),
+    username    varchar(40) PRIMARY KEY,
     password    varchar,
     salt        varchar,
+    clinic_id   integer,
     admin       boolean,
     CONSTRAINT fk_clinic
         FOREIGN KEY(clinic_id)
             REFERENCES TriageData.Clinic(id)
 );
 CREATE TABLE TriageData.TriageClasses (
-    id          SERIAL PRIMARY KEY,
     clinic_id   integer,
     severity    integer,
     name        varchar(255),
     duration    integer,
     proportion  float,
+    PRIMARY KEY (clinic_id, severity)
     CONSTRAINT fk_clinic
         FOREIGN KEY(clinic_id)
             REFERENCES TriageData.Clinic(id)
