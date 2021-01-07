7478bdfa3d161b69b94db04b90b9db4dbda314c1
Shawn <shawnesseltine@hotmail.com>
2021-01-06 23:38:19 -0500

    add models to the front facing api

--- api/triage_api.py
+++ api/triage_api.py
@@ -4,11 +4,14 @@
 from flask import Flask
 from flask_restful import Api
 from resources.predict import Predict
+from resources.models import Models, Use
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Predict, '/predict')
+api.add_resource(Models, '/models')
+api.add_resource(Use, '/models/use')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
