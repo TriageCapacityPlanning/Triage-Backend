e1ac5bbd59b76baf5f56791a780d9dbb226c49c7
Shawn <shawnesseltine@hotmail.com>
2021-01-12 15:47:03 -0500

    move to new database controller

--- api/resources/update_triage_classes.py
+++ api/resources/update_triage_classes.py
@@ -1,15 +1,14 @@
+from api.common.database_interaction import DataBase
 from flask_restful import Resource
 from flask import request
-import psycopg2
 from webargs.flaskparser import parser
-from webargs import fields, validate
-import json
+from webargs import fields
 
 
 class UpdateTriageClasses(Resource):
-    DATA = {
+    DATABASE_DATA = {
         'database': 'triage',
-        'user': 'admin',
+        'user': 'triageClassHandler',
         'password': 'password',
         'host': 'localhost',
         'port': '5432'
@@ -25,72 +24,42 @@ class UpdateTriageClasses(Resource):
 
     def get(self):
         # Validate input arguments.
-        args = parser.parse(self.arg_schema_get, request,
-                            location='querystring')
-
+        args = parser.parse(self.arg_schema_get, request, location='querystring')
         # Retrieve triage classes from database
         triage_classes = self.get_triage_classes(args['clinic-id'])
-
         # API Response
         return {'status': 200, 'classes': triage_classes}
 
     def put(self):
         # Validate input arguments
         args = parser.parse(self.arg_schema_put, request)
-
         # Update triage class in database
         self.update_triage_class(args['triage-class'])
-
         # API Response
         return {'status': 200, 'updated': args['triage-class']}
 
     def get_triage_classes(self, clinic_id):
         # Keys for response
-        keys = ['clinic_id', 'severity',
-                'name', 'duration', 'proportion']
-
-        # Establish database connection
-        db = psycopg2.connect(
-            database=self.DATA['database'],
-            user=self.DATA['user'],
-            password=self.DATA['password'],
-            host=self.DATA['host'],
-            port=self.DATA['port']
-        )
-
-        # Query for data
-        cur = db.cursor()
-        cur.execute(f"SELECT clinic_id, severity, name, duration, proportion \
-                      FROM triagedata.triageclasses \
-                      WHERE clinic_id=%(clinic_id)s",
-                    {
-                        'clinic_id': clinic_id
-                    })
-        rows = cur.fetchall()
-        db.close()
-
+        keys = ['clinic_id', 'severity', 'name', 'duration', 'proportion']
+        # Establish database connection and get the data
+        db = DataBase(self.DATABASE_DATA)
+        rows = db.select(f"SELECT clinic_id, severity, name, duration, proportion \
+                           FROM triagedata.triageclasses \
+                           WHERE clinic_id={clinic_id}")
         # Return data
         return [dict(zip(keys, values)) for values in rows]
 
     def update_triage_class(self, triage_class):
         # Establish database connection
-        db = psycopg2.connect(
-            database=self.DATA['database'],
-            user=self.DATA['user'],
-            password=self.DATA['password'],
-            host=self.DATA['host'],
-            port=self.DATA['port']
-        )
-
+        db = DataBase(self.DATABASE_DATA)
         # Insert or update information
-        cur = db.cursor()
-        cur.execute(f"INSERT INTO triagedata.triageclasses (clinic_id, severity, name, duration, proportion) \
-                      VALUES(%(clinic_id)s, \
-                             %(severity)s, \
-                             %(name)s, \
-                             %(duration)s, \
-                             %(proportion)s) \
-                      ON CONFLICT ON CONSTRAINT pk DO UPDATE \
+        db.insert(f"INSERT INTO triagedata.triageclasses (clinic_id, severity, name, duration, proportion) \
+                    VALUES(%(clinic_id)s, \
+                           %(severity)s, \
+                           %(name)s, \
+                           %(duration)s, \
+                           %(proportion)s) \
+                    ON CONFLICT ON CONSTRAINT pk DO UPDATE \
                         SET name = %(name)s, \
                             duration = %(duration)s, \
                             proportion = %(proportion)s",
@@ -101,5 +70,3 @@ class UpdateTriageClasses(Resource):
                         'duration': triage_class['duration'],
                         'proportion': triage_class['proportion']
                     })
-        db.commit()
-        db.close()
