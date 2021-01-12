1332c81cce34a493a47d36a50d74de2ff6f7a1c2
Shawn <shawnesseltine@hotmail.com>
2021-01-12 13:31:01 -0500

    merge cleanup (whoops)

--- api/triage_api.py
+++ api/triage_api.py
@@ -4,25 +4,19 @@
 from flask import Flask
 from flask_restful import Api
 from resources.predict import Predict
-<<<<<<< HEAD
 from resources.update_triage_classes import UpdateTriageClasses
 from resources.models import Models, Use
-=======
 import resources.upload as Upload
->>>>>>> upload endpoints boilerplate
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Predict, '/predict')
-<<<<<<< HEAD
 api.add_resource(Models, '/models')
 api.add_resource(Use, '/models/use')
 api.add_resource(UpdateTriageClasses, '/classes')
-=======
 api.add_resource(Upload.Waitlist, '/upload/waitlist')
 api.add_resource(Upload.PastAppointments, '/upload/past-appointments')
->>>>>>> upload endpoints boilerplate
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
