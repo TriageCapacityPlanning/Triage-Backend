e668b50f0c9315da6c3beec023379efbff42b642
Daniel <rubinstd@mcmaster.ca>
2021-01-12 15:52:42 -0500

    Implemented prediction endpoint with TriageController and DataFrame

---
+++ api/common/controller/TriageController.py
@@ -0,0 +1,89 @@
+import psycopg2
+import datetime
+from common.controller.DataFrame import DataFrame
+
+class TriageController:
+    # Database connection information
+    DATA = {
+        'database': 'triage',
+        'user': 'admin',
+        'password': 'password',
+        'host': 'localhost',
+        'port': '5432'
+    }
+
+    # Constructor
+    def __init__(self, params):
+        self.params = params
+
+    def predict(self):
+        # Retrieve previous years referal data from database
+        initial_referal_data = self.get_initial_referal_data(self.params['start-date'])
+        
+        # Format and feed historic referal data into ML model to predict future referals
+        initial_prediction_data = self.format_referal_data(initial_referal_data)
+        predictions = self.get_predictions(self.params['intervals'], initial_prediction_data)
+
+        # Retrieve a distribution prediction of patients by triage class
+        distribution = self.get_triage_class_distribution(self.params['clinic-settings'])
+
+        # Create the dataframe and run simulation
+        data_frame = DataFrame(self.params['intervals'], predictions, distribution)
+        # Note: Currently returns just the predictions variable 
+        # but will return simulation results connected to the simulation module
+        simulation_results = {
+            'interval': predictions,
+            'total': predictions
+        }
+        
+        return simulation_results
+        
+    def get_initial_referal_data(self, start_date):
+        # Calculate the (start date - a year) to search the database for relevant historic data
+        previous_year_date = datetime.datetime.strptime(start_date, '%Y-%m-%d') - datetime.timedelta(days=365)
+        
+        # Establish database connection
+        db = psycopg2.connect(
+            database=self.DATA['database'],
+            user=self.DATA['user'],
+            password=self.DATA['password'],
+            host=self.DATA['host'],
+            port=self.DATA['port']
+        )
+
+        # Query for referal data from previous year
+        cur = db.cursor()
+        cur.execute(f"SELECT date_received \
+                      FROM triagedata.historicdata \
+                      WHERE date_received < %(previous_year_date)s::date \
+                      ORDER BY date_received DESC \
+                      FETCH FIRST 31 ROWS ONLY",
+                      {
+                          'previous_year_date': previous_year_date
+                      })
+        rows = cur.fetchall()
+        db.close()
+
+        # Return results
+        return [date_seen[0] for date_seen in rows]
+
+    def format_referal_data(self, referal_data):
+        # Find the difference in days between historic referal datapoints
+        return [(next_referal - previous_referal).days
+                for previous_referal, next_referal
+                in zip(referal_data[:-1], referal_data[1:])]
+
+    def get_predictions(self, intervals, initial_prediction_data):
+        # Return a list of predictions
+        # Note: Currently hardcoded but will change when connected to the ML module
+        return [(1, 1) for x in range(len(intervals))]
+
+    def get_triage_class_distribution(self, clinic_settings):
+        # Return a distribution of patient referals by triage class 
+        # Note: Currently hard coded but will be calculated once more data is provided by the client
+        return {
+            1: 0.5,
+            2: 0.2,
+            3: 0.2,
+            4: 0.1
+        }
--- api/resources/predict.py
+++ api/resources/predict.py
@@ -3,30 +3,73 @@ from flask import request
 import psycopg2
 from webargs.flaskparser import parser
 from webargs import fields, validate
-
-predict_args = {
-    "clinic-id": fields.Int(required=True),
-    "start-date": fields.String(required=True),
-    "end-date": fields.String(required=True),
-    "intervals": fields.List(fields.String),
-    "confidence": fields.Float(missing=0.95),
-    "num-sim-runs": fields.Int(missing=1000),
-    "waitlist": fields.Raw(missing=[])
-}
+from common.controller.TriageController import TriageController
 
 class Predict(Resource):
+    # Database connection information
+    DATA = {
+        'database': 'triage',
+        'user': 'admin',
+        'password': 'password',
+        'host': 'localhost',
+        'port': '5432'
+    }
+
+    # API input schema
+    arg_schema_get = {
+        "clinic-id": fields.Int(required=True),
+        "start-date": fields.String(required=True),
+        "end-date": fields.String(required=True),
+        "intervals": fields.List(fields.Raw(), missing=[]),
+        "confidence": fields.Float(missing=0.95),
+        "num-sim-runs": fields.Int(missing=1000),
+        "waitlist": fields.Raw(missing=[])
+    }
+
     def get(self):
         # Validate input arguments.
-        args = parser.parse(predict_args, request, location='querystring')
+        args = parser.parse(self.arg_schema_get, request, location='querystring')
 
         # Retrieve clinic settings
-        clinic_settings = self.getClinicSettings(args['clinic-id'])
+        args['clinic-settings'] = self.get_clinic_settings(args['clinic-id'])
 
-        # Trigger Simulation
-        pass
+        # Instantiate TriageController
+        triage_controller = TriageController(args)
+        predictions = triage_controller.predict()
 
-        # Return result
-        return args
+        # API response
+        return {
+            'url': request.url,
+            'intervaled_slot_predictions': predictions['interval'],
+            'number_intervals': len(args['intervals']),
+            'slot_predictions': predictions['total']
+        }
 
-    def getClinicSettings(self, clinic_id):
-        return {}
+
+    def get_clinic_settings(self, clinic_id):
+        # Keys for response
+        keys = ['clinic_id', 'severity',
+                'name', 'duration', 'proportion']
+
+        # Establish database connection
+        db = psycopg2.connect(
+            database=self.DATA['database'],
+            user=self.DATA['user'],
+            password=self.DATA['password'],
+            host=self.DATA['host'],
+            port=self.DATA['port']
+        )
+
+        # Query for data
+        cur = db.cursor()
+        cur.execute(f"SELECT clinic_id, severity, name, duration, proportion \
+                      FROM triagedata.triageclasses \
+                      WHERE clinic_id=%(clinic_id)s",
+                    {
+                        'clinic_id': clinic_id
+                    })
+        rows = cur.fetchall()
+        db.close()
+
+        # Return data
+        return [dict(zip(keys, values)) for values in rows]
