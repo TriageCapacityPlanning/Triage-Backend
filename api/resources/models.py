cf42557f917f74f0ee304330c791317d3ab4a673
Shawn <shawnesseltine@hotmail.com>
2021-01-12 15:32:02 -0500

    implement the new db interacgtion class and users

--- api/resources/models.py
+++ api/resources/models.py
@@ -1,50 +1,58 @@
+from api.common.database_interaction import DataBase
 from flask_restful import Resource
 from flask import request
-import psycopg2
+from webargs.flaskparser import parser
+from webargs import fields
 
 class Models(Resource):
-    def get(self):
-        args = request.args.to_dict()
-        try:
-            clinic_id = int(args['clinic-id'])
-        except:
-            return {'status': 400}
-        db = psycopg2.connect(
-                database="triage", 
-                user="admin", 
-                password="docker", 
-                host="localhost", 
-                port="5432"
-            )
-        cur = db.cursor()
-        cur.execute(f"SELECT id, accuracy, to_char(created,'DD-MM-YYYY'), in_use \
-                      FROM triagedata.models \
-                      WHERE clinic_id={clinic_id}")
-        rows = cur.fetchall()
-        db.close()
-        return {'status': 200, 'models': [('id', 'accuracy', 'created', 'in_use')] + rows}
+    DATABASE_DATA = {
+        'database': 'triage',
+        'user': 'modelHandler',
+        'password': 'password',
+        'host': 'localhost',
+        'port': '5432'
+    }
 
-class Use(Resource):
+    arg_schema_get = {
+        'clinic-id': fields.Int(required=True)
+    }
+
+    arg_schema_patch = {
+        'clinic-id': fields.Int(required=True),
+        'model-id': fields.Int(required=True)
+    }
+
+    def get(self):
+        # Validate input arguments.
+        args = parser.parse(self.arg_schema_get, request,
+                            location='querystring')
+        # Retrieve triage classes from database
+        models = self.get_clinic_models(args['clinic-id'])
+        # API Response
+        return {'status': 200, 'models': models}
+
     def patch(self):
-        args = request.args.to_dict()
-        try:
-            clinic_id = int(args['clinic-id'])
-            model_id = int(args['model-id'])
-        except:
-            return {'status': 400}
-        db = psycopg2.connect(
-                database="triage", 
-                user="admin", 
-                password="docker", 
-                host="localhost", 
-                port="5432"
-            )        
-        cur = db.cursor()
-        cur.execute(f"UPDATE triagedata.models \
-                      SET in_use = (CASE WHEN id={model_id} THEN true \
-                                         ELSE false \
-                                    END) \
-                      WHERE clinic_id={clinic_id}")
-        db.commit()
-        db.close()
+        # Validate input arguments.
+        args = parser.parse(self.arg_schema_patch, request,
+                            location='querystring')
+        self.get_clinic_models(args['clinic-id'], args['model-id'])
         return {'status': 200}
+
+    def set_active_model(self, clinic_id, model_id):
+        db = DataBase(self.DATABASE_DATA)
+        db.update(f"UPDATE triagedata.models \
+                    SET in_use = (CASE WHEN id={model_id} THEN true \
+                                       ELSE false \
+                                  END) \
+                    WHERE clinic_id={clinic_id}")
+
+    def get_clinic_models(self, clinic_id):
+        # Keys for response
+        keys = ['id', 'accuracy', 'created', 'in_use']
+        # Establish database connection and get the data
+        db = DataBase(self.DATABASE_DATA)
+        rows = db.select(f"SELECT id, accuracy, to_char(created,'DD-MM-YYYY'), in_use \
+                           FROM triagedata.models \
+                           WHERE clinic_id={clinic_id}")
+        # Return data
+        return [dict(zip(keys, values)) for values in rows]
