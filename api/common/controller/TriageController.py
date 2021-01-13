6ab662657609d46f0a060888d77bb6f2b8240033
Shawn <shawnesseltine@hotmail.com>
2021-01-12 21:34:31 -0500

    pep8 compliance and move to DataBase for db

--- api/common/controller/TriageController.py
+++ api/common/controller/TriageController.py
@@ -1,10 +1,11 @@
-import psycopg2
 import datetime
 from common.controller.DataFrame import DataFrame
+from common.database_interaction import DataBase
+
 
 class TriageController:
     # Database connection information
-    DATA = {
+    DATABASE_DATA = {
         'database': 'triage',
         'user': 'triage_controller',
         'password': 'password',
@@ -18,51 +19,48 @@ class TriageController:
 
     def predict(self):
         # Retrieve previous years referal data from database
-        initial_referal_data = self.get_initial_referal_data(self.params['start-date'])
-        
+        initial_referal_data = self.get_initial_referal_data(
+            self.params['start-date'])
+
         # Format and feed historic referal data into ML model to predict future referals
-        initial_prediction_data = self.format_referal_data(initial_referal_data)
-        predictions = self.get_predictions(self.params['intervals'], initial_prediction_data)
+        initial_prediction_data = self.format_referal_data(
+            initial_referal_data)
+        predictions = self.get_predictions(
+            self.params['intervals'], initial_prediction_data)
 
         # Retrieve a distribution prediction of patients by triage class
-        distribution = self.get_triage_class_distribution(self.params['clinic-settings'])
+        distribution = self.get_triage_class_distribution(
+            self.params['clinic-settings'])
 
         # Create the dataframe and run simulation
-        data_frame = DataFrame(self.params['intervals'], predictions, distribution)
-        # Note: Currently returns just the predictions variable 
+        data_frame = DataFrame(
+            self.params['intervals'], predictions, distribution)
+        # Note: Currently returns just the predictions variable
         # but will return simulation results connected to the simulation module
         simulation_results = {
             'interval': predictions,
             'total': predictions
         }
-        
+
         return simulation_results
-        
+
     def get_initial_referal_data(self, start_date):
         # Calculate the (start date - a year) to search the database for relevant historic data
-        previous_year_date = datetime.datetime.strptime(start_date, '%Y-%m-%d') - datetime.timedelta(days=365)
-        
+        previous_year_date = datetime.datetime.strptime(
+            start_date, '%Y-%m-%d') - datetime.timedelta(days=365)
+
         # Establish database connection
-        db = psycopg2.connect(
-            database=self.DATA['database'],
-            user=self.DATA['user'],
-            password=self.DATA['password'],
-            host=self.DATA['host'],
-            port=self.DATA['port']
-        )
+        db = DataBase(self.DATABASE_DATA)
 
         # Query for referal data from previous year
-        cur = db.cursor()
-        cur.execute(f"SELECT date_received \
-                      FROM triagedata.historicdata \
-                      WHERE date_received < %(previous_year_date)s::date \
-                      ORDER BY date_received DESC \
-                      FETCH FIRST 31 ROWS ONLY",
-                      {
-                          'previous_year_date': previous_year_date
-                      })
-        rows = cur.fetchall()
-        db.close()
+        rows = db.select(f"SELECT date_received \
+                           FROM triagedata.historicdata \
+                           WHERE date_received < %(previous_year_date)s::date \
+                           ORDER BY date_received DESC \
+                           FETCH FIRST 31 ROWS ONLY",
+                         {
+                             'previous_year_date': previous_year_date
+                         })
 
         # Return results
         return [date_seen[0] for date_seen in rows]
@@ -79,7 +77,7 @@ class TriageController:
         return [(1, 1) for x in range(len(intervals))]
 
     def get_triage_class_distribution(self, clinic_settings):
-        # Return a distribution of patient referals by triage class 
+        # Return a distribution of patient referals by triage class
         # Note: Currently hard coded but will be calculated once more data is provided by the client
         return {
             1: 0.5,
