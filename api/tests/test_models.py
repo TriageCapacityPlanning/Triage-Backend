f2e3f7df1bb390dc5a66b9ff788c17f89dd43913
Shawn <shawnesseltine@hotmail.com>
2021-03-29 23:00:49 -0400

    comment model tests

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -6,25 +6,43 @@ from api.tests.common import generate_token
 import json
 
 
-class TestModels:
+class TestModelsAPI:
+    """
+    The `TestModelsAPI` class contains acceptance tests for models API functions.
+    """
     def setup_class(self):
+        """
+        Test setup that occurs once before all tests are run.
+        """
         self.test_client = create_app().test_client()
         self.token = generate_token('username', 1)
         self.endpoint = VERSION_PREFIX + '/models'
 
     def test_get_missing_inputs(self):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         input_mock = {}
         response = self.test_client.get(self.endpoint, headers={'token': self.token}, query_string=input_mock)
 
         assert response.status_code == 422
 
     def test_get_incorrect_input_type(self):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         input_mock = {'clinic-id': '"1"'}
         response = self.test_client.get(self.endpoint, headers={'token': self.token}, query_string=input_mock)
 
         assert response.status_code == 422
 
     def test_get_triage_models_success_singleton(self, mocker):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         triage_models_mock = [
             {
                 'id': 1,
@@ -43,6 +61,10 @@ class TestModels:
         assert json.loads(response.data)['models'] == triage_models_mock
 
     def test_get_triage_models_success_multiple(self, mocker):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         triage_models_mock = [
             {
                 'id': 1,
@@ -73,6 +95,10 @@ class TestModels:
         assert json.loads(response.data)['models'] == triage_models_mock
 
     def test_get_models_requires_header_with_token(self, mocker):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         mocker.patch('api.resources.models.Models.get_clinic_models')
         model_id = 1
         input_mock = {'clinic-id': 1, 'model-id': model_id}
@@ -87,24 +113,40 @@ class TestModels:
         assert response.status_code == 401
 
     def test_patch_missing_inputs(self):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         input_mock = {}
         response = self.test_client.patch(self.endpoint, headers={'token': self.token}, query_string=input_mock)
 
         assert response.status_code == 422
 
     def test_patch_incorrect_input_type_clinic_id(self):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         input_mock = {'clinic-id': '"1"', 'model-id': 1}
         response = self.test_client.patch(self.endpoint, headers={'token': self.token}, query_string=input_mock)
 
         assert response.status_code == 422
 
     def test_patch_incorrect_input_type_model_id(self):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         input_mock = {'clinic-id': 1, 'model-id': '"1"'}
         response = self.test_client.patch(self.endpoint, headers={'token': self.token}, query_string=input_mock)
 
         assert response.status_code == 422
 
     def test_patch_models_success_singleton(self, mocker):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         mocker.patch(
             'api.resources.models.Models.set_active_model')
         model_id = 1
@@ -114,6 +156,10 @@ class TestModels:
         assert json.loads(response.data)['active_model'] == model_id
 
     def test_patch_models_requires_header_with_token(self, mocker):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         mocker.patch('api.resources.models.Models.set_active_model')
         model_id = 1
         input_mock = {'clinic-id': 1, 'model-id': model_id}
@@ -121,6 +167,10 @@ class TestModels:
         assert response.status_code == 401
 
     def test_patch_models_requires_valid_token(self, mocker):
+        """
+        Test Type: Acceptance
+        Test Purpose: Tests Requirement
+        """
         mocker.patch('api.resources.models.Models.set_active_model')
         model_id = 1
         input_mock = {'clinic-id': 1, 'model-id': model_id}
@@ -129,10 +179,20 @@ class TestModels:
 
 
 class TestModelsUnit:
+    """
+    The `TestModelsUnit` class contains unit tests for models functions.
+    """
     def setup_class(self):
+        """
+        Test setup that occurs once before all tests are run.
+        """
         self.models = Models()
 
     def test_get_triage_models_database_error(self, mocker):
+        """
+        Test Type: Unit
+        Test Purpose: Tests that an error is thrown if a the database connection fails.
+        """
         mocker.patch('api.common.database_interaction.DataBase.select',
                      side_effect=RuntimeError('Database error'))
 
@@ -140,6 +200,10 @@ class TestModelsUnit:
             self.models.get_clinic_models(3)
 
     def test_get_triage_models_empty_data_error(self, mocker):
+        """
+        Test Type: Unit
+        Test Purpose: Tests that an error is thrown if the database returns no rows.
+        """
         mocker.patch('api.common.database_interaction.DataBase.select',
                      return_value=[])
 
@@ -147,6 +211,10 @@ class TestModelsUnit:
             self.models.get_clinic_models(3)
 
     def test_get_triage_models_success_singleton(self, mocker):
+        """
+        Test Type: Unit
+        Test Purpose: Tests that a singleton is returned if a single tuple was found in the db.
+        """
         database_response_mock = [[1, .95, '05-05-2020', 'true']]
         result_expected = [
             {
@@ -163,6 +231,10 @@ class TestModelsUnit:
         assert self.models.get_clinic_models(3) == result_expected
 
     def test_get_triage_models_success_multiple(self, mocker):
+        """
+        Test Type: Unit
+        Test Purpose: Tests that multiple rows are returned if multiple tuples are found in the db.
+        """
         database_response_mock = [
             [1, .95, '05-05-2020', 'false'],
             [2, .92, '05-06-2020', 'false'],
@@ -195,6 +267,10 @@ class TestModelsUnit:
         assert self.models.get_clinic_models(3) == result_expected
 
     def test_set_active_triage_model_database_error(self, mocker):
+        """
+        Test Type: Unit
+        Test Purpose: Tests that an error is thrown if a the database connection fails.
+        """
         mocker.patch('api.common.database_interaction.DataBase.update',
                      side_effect=RuntimeError('Database error'))
 
@@ -202,6 +278,10 @@ class TestModelsUnit:
             self.models.set_active_model(3, 1)
 
     def test_set_active_triage_model_database_success(self, mocker):
+        """
+        Test Type: Unit
+        Test Purpose: Tests that no error is thrown if a the update is successful.
+        """
         mocker.patch('api.common.database_interaction.DataBase.update')
 
         self.models.set_active_model(3, 1)
